f6c8b77a7139cac35fbaef8c1abc1780bdd956d1
jonas <jonas@Znuten.(none)>
2012-11-04 12:28:02 +0100

    Initial commit

---
+++ tutorial/spiders/swedbank_spider.py
@@ -0,0 +1,12 @@
+from scrapy.spider import BaseSpider
+
+
+class SwedbankSpider(BaseSpider):
+    name = "swedb"
+    
+    start_urls = [
+        ""
+        ]
+    
+    def parse(self, response):
+        

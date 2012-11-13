7d60d5b27e872128ea7c996bacd91f2668f40630
jonasH <jonas.hesselryd@gmail.com>
2012-11-13 21:17:36 +0100

    Fixed date to be crawled, refactred to more general methods

--- tutorial/spiders/blocketspider.py
+++ tutorial/spiders/blocketspider.py
@@ -19,18 +19,39 @@ class BlocketSpider(BaseSpider):
             return hxs.select(xpath)
 
     def print_to_file(self,text):
+        """Print a text to a file that is specified for this class"""
         output_file = self.output_file
         output_file.write(text)
-        output_file.write(unicode('</br>\n'))
+        output_file.write(unicode('\n'))
+
+    def get_relative_item(self,item,xpath):
+        """Gets the path from a html path selector, returns False if no hit"""
+        result = item.select(xpath).extract()
+        if result.__len__() == 0:
+            return False
+        return result[0]
+        
+    
+    
 
     def iterate_blocket_items(self,items):
         for item in items:
-            price = item.select('div/p/text()').extract()[0]
-            # cleaning the tags and whitespace away
-            price = re.sub(':.*','', price)
-            price = re.sub('\s','', price)
-            # concat the strings with price and text
-            text = item.select('div/a/text()').extract()[0] + u' ' + price
+            text = ""
+            price = self.get_relative_item(item,'div/p/text()')
+            if price:
+                price = re.sub('\s','',price)
+                price = re.sub('\:.*','',price)
+                text = price + text
+            else:
+                continue
+            name = self.get_relative_item(item,'div/a/text()')
+            if name:
+                text = name + u' ' + text
+            date = self.get_relative_item(item,'div/div/text()')
+            if date:
+                text = date + u' ' + text
+            
+            
             # if the item costs more
             if price and int(price) > self.min_cost and int(price) < self.max_cost:
                 self.print_to_file(text)

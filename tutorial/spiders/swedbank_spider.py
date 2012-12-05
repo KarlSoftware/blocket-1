864a8f82984206a672d56103e9350c6606424d29
jonasH <jonas.hesselryd@gmail.com>
2012-12-05 20:00:29 +0100

    Added logging in a file, also refined the denyrules to filer away shiet

--- tutorial/spiders/swedbank_spider.py
+++ tutorial/spiders/swedbank_spider.py
@@ -10,11 +10,16 @@ class SwedbankSpiderSpider(CrawlSpider):
     start_urls = ['http://fonder.swedbankrobur.se/robur/site/query.action']
 
     rules = (
-        Rule(SgmlLinkExtractor(allow=r'\.htm'), callback='parse_iframe', follow=True),
-    )
+        Rule(SgmlLinkExtractor(allow=r'fonder', deny=('.*nyheter.*','fonder\.')), callback='parse_iframe', follow=False),
+        )
 
+
+    
     def parse_iframe(self, response):
-        self.log('awyeah!')
+        output_file = codecs.open("/home/jonas/data/scrapy/swedbank.txt",
+                                  encoding='utf-8', mode='a')
+        output_file.write(response.url + '\n')
+        output_file.close()
 
     def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
